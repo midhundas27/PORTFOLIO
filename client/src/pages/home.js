5688d5b36360f45ed34ce57447ff2d1e9133cea8
midhundas27 <dasmidhun27@gmail.com>
2025-02-18 22:29:07 +0530

    main and about setup done

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../styles/home.css';
+import Header from '../components/header';
+import Main from '../components/main';
+import About from '../components/about';
 
 
 function Home() {
 
   return (
-    <div></div>
+    <div>
+    <Header></Header>
+    <Main></Main>
+    <About></About>
+    </div>
   );
 }
 
